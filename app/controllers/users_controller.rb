7264e1a437d98211a14aaaaba1e0ade498b1e7e4
UsmanNadeem <usmanone@gmail.com>
2015-05-11 20:06:36 +0500

    fix user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
 
 	def show
 		@currUser = User.find_by_id(params[:id])
-		session[:user_id] = params[:id]
+		# session[:user_id] = params[:id]
 		if @currUser != nil
 			@allevents = @currUser.events
+		else
+			flash[:notice] = "No such user"
+			redirect_to user_path session[:user_id]
 		end
 		#random coommmetn
 	end

feaf5e73b6cb84983fe8f376e879e5b8d24e5261
UmairaSajjad <umairasajjad@gmail.com>
2015-05-25 01:29:45 -0300

    check this

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,72 +42,72 @@ class UsersController < ApplicationController
 
 	end
 
-	def setMeeting
-		currUser_events  = User.find_by_id(params[:id]).events
-		otherUser_events = User.find_by_name(params[:friend_id]).events
-		all_events = currUser_events + otherUser_events
-		curDate = params[:curr_date]
+	# def setMeeting
+	# 	currUser_events  = User.find_by_id(params[:id]).events
+	# 	otherUser_events = User.find_by_name(params[:friend_id]).events
+	# 	all_events = currUser_events + otherUser_events
+	# 	curDate = params[:curr_date]
 
-		year = curDate[6] + curDate[7] + curDate[8]+ curDate[9]
-		month = curDate[3] + curDate[4]
-		day = curDate[0] + curDate[1]
-		@currDate = day + "/" + month + "/" + year
-		today_events = []
-		free_time = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true]
+	# 	year = curDate[6] + curDate[7] + curDate[8]+ curDate[9]
+	# 	month = curDate[3] + curDate[4]
+	# 	day = curDate[0] + curDate[1]
+	# 	@currDate = day + "/" + month + "/" + year
+	# 	today_events = []
+	# 	free_time = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true]
 		
-		# currUser_events.each { |x|
-		# 	if x.starttime.day.to_s == day && x.endtime.day.to_s == day
-		# 		today_events << x
-		# 	end
-		# }
-		# otherUser_events.each { |x| 
-		# 	if x.starttime.day.to_s == day && x.endtime.day.to_s == day
-		# 		today_events << x
-		# 	end
-		# }
-		all_events.each { |event| 
-			if event.starttime.day.to_s == day && event.endtime.day.to_s == day
-				todays_event = event
-				((todays_event.starttime.hour.to_i) .. (todays_event.endtime.hour.to_i)).each do |i| 
-					free_time[i] = false
-				end
-			end
-		}
-		#VFDJVJKDFJVKJDFJV
-		# logger.debug("TODAYS EVENTS: ")
-		# logger.debug(today_events)
-		# today_events.each do |x|
-		# 	((x.starttime.hour.to_i) .. (x.endtime.hour.to_i)).each do |i| 
-		# 		# logger.debug("SEE THIS BITCHES")
-		# 		# logger.debug(i)
-		# 		free_time[i] = false
-		# 	end
-		# end
-		@free_slots = []
-		for i in 0..23
-				if free_time[i] == true
-					@free_slots << i
-				end
-		end
-		temp = ""
-		# logger.debug("FREE_SLOTSSS")
-		# logger.debug(@free_slots)
-		# logger.debug(@free_slots.size)
-		@free_slots_two = []
-		for i in 0..(@free_slots.size-1)
-			if i == 0
-				temp = @free_slots[i].to_s + ":00 - "
-			end
-			# logger.debug("@free_slots[i] + 1 :")
-			# logger.debug(@free_slots[i] + 1)
-			# logger.debug("@free_slots[i+1]  :")
-			# logger.debug(@free_slots[i+1])
+	# 	# currUser_events.each { |x|
+	# 	# 	if x.starttime.day.to_s == day && x.endtime.day.to_s == day
+	# 	# 		today_events << x
+	# 	# 	end
+	# 	# }
+	# 	# otherUser_events.each { |x| 
+	# 	# 	if x.starttime.day.to_s == day && x.endtime.day.to_s == day
+	# 	# 		today_events << x
+	# 	# 	end
+	# 	# }
+	# 	all_events.each { |event| 
+	# 		if event.starttime.day.to_s == day && event.endtime.day.to_s == day
+	# 			todays_event = event
+	# 			((todays_event.starttime.hour.to_i) .. (todays_event.endtime.hour.to_i)).each do |i| 
+	# 				free_time[i] = false
+	# 			end
+	# 		end
+	# 	}
+	# 	#VFDJVJKDFJVKJDFJV
+	# 	# logger.debug("TODAYS EVENTS: ")
+	# 	# logger.debug(today_events)
+	# 	# today_events.each do |x|
+	# 	# 	((x.starttime.hour.to_i) .. (x.endtime.hour.to_i)).each do |i| 
+	# 	# 		# logger.debug("SEE THIS BITCHES")
+	# 	# 		# logger.debug(i)
+	# 	# 		free_time[i] = false
+	# 	# 	end
+	# 	# end
+	# 	@free_slots = []
+	# 	for i in 0..23
+	# 			if free_time[i] == true
+	# 				@free_slots << i
+	# 			end
+	# 	end
+	# 	temp = ""
+	# 	# logger.debug("FREE_SLOTSSS")
+	# 	# logger.debug(@free_slots)
+	# 	# logger.debug(@free_slots.size)
+	# 	@free_slots_two = []
+	# 	for i in 0..(@free_slots.size-1)
+	# 		if i == 0
+	# 			temp = @free_slots[i].to_s + ":00 - "
+	# 		end
+	# 		# logger.debug("@free_slots[i] + 1 :")
+	# 		# logger.debug(@free_slots[i] + 1)
+	# 		# logger.debug("@free_slots[i+1]  :")
+	# 		# logger.debug(@free_slots[i+1])
 				
-			if @free_slots[i+1] != (@free_slots[i]+1)
-				temp = temp + @free_slots[i].to_s + ":00"
-				@free_slots_two << temp
-				temp = @free_slots[i+1].to_s + ":00 - "
-			end
-		end
-	end
+	# 		if @free_slots[i+1] != (@free_slots[i]+1)
+	# 			temp = temp + @free_slots[i].to_s + ":00"
+	# 			@free_slots_two << temp
+	# 			temp = @free_slots[i+1].to_s + ":00 - "
+	# 		end
+	# 	end
+	# end
 end
